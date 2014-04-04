d9a1c4080758b393b0a2ab3d2ab546dd13014b85
Gary Liu <garyliu@pivotallabs.com>
2014-04-04 17:38:12 +0800

    two nat boxes and a controller

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-Usage
-=====
+How it works
+------------
+2 subnets:
+- public
+- private
 
+4 instances:
+nat box A
+nat box B
+controller
+server A
+
+2 Elastic IPs
+- 1 for NAT egress address
+- 1 for controller
+
+2 route tables
+- rt A points to nat box A
+- rt B points to nat box B
+
+
+
+Usage
+------
 1. `. bootstrap_nat_ha.sh`
-1. ssh into the jumpbox: `ssh -A ec2-user@$eip_jb`
+1. ssh into the controller: `ssh -A ec2-user@$eip_c`
 1. after done playing, `nuke`
--- bootstrap_nat_ha.sh
+++ bootstrap_nat_ha.sh
@@ -7,128 +7,111 @@ az=$BOSH_VPC_PRIMARY_AZ
 nat_ami=ami-ad227cc4 #amzn-ami-vpc-nat-pv-2013.09.0.x86_64-ebs
 server_ami=ami-2f726546 #Amazon Linux AMI 2014.03
 
-function retry() { while ! eval $@; do sleep 10; echo -e "retry...\n"; done } 
-alias ec2='aws ec2'
+function retry() { i=3; while [ $i -ge 1 ] ; do  eval $@ && break; i=$((i-1)); sleep 10; echo -e "retry...\n"; done } 
+alias ec2='retry aws ec2'
+alias ec2t='retry aws --output text ec2'
+alias jq='jq -r'
 
 #vpc
 echo "creating vpc for Nat HA Demo"
 echo "======================="
-vpc=`ec2 create-vpc --cidr-block 10.8.0.0/16 |jq .Vpc.VpcId -r`
+vpc=`ec2 create-vpc --cidr-block 10.8.0.0/16 |jq .Vpc.VpcId `
 echo $vpc 
 
 #subnets
 echo "creating subnets"
 echo "======================="
-subnet_pub_a=`ec2 create-subnet --cidr-block 10.8.0.0/24 --vpc-id $vpc --availability-zone $az | jq .Subnet.SubnetId -r` 
+subnet_pub_a=`ec2 create-subnet --cidr-block 10.8.0.0/24 --vpc-id $vpc --availability-zone $az | jq .Subnet.SubnetId ` 
 echo $subnet_pub_a
 
-subnet_prv_a=`ec2 create-subnet --cidr-block 10.8.2.0/24 --vpc-id $vpc --availability-zone $az | jq .Subnet.SubnetId -r`
+subnet_prv_a=`ec2 create-subnet --cidr-block 10.8.2.0/24 --vpc-id $vpc --availability-zone $az | jq .Subnet.SubnetId `
 echo $subnet_prv_a
 
-ec2 create-tags --resources $vpc          --tags Key=Name,Value=Nats-HA-Demo
-ec2 create-tags --resources $subnet_pub_a --tags Key=Name,Value=Public-a
-ec2 create-tags --resources $subnet_prv_a --tags Key=Name,Value=Private-a
+ec2t create-tags --resources $vpc          --tags Key=Name,Value=Nats-HA-Demo
+ec2t create-tags --resources $subnet_pub_a --tags Key=Name,Value=Public-a
+ec2t create-tags --resources $subnet_prv_a --tags Key=Name,Value=Private-a
 
 #igw
-igw=`ec2 create-internet-gateway | jq .InternetGateway.InternetGatewayId -r`
+igw=`ec2 create-internet-gateway | jq .InternetGateway.InternetGatewayId `
 echo $igw
-ec2 attach-internet-gateway --internet-gateway-id $igw --vpc-id $vpc 
+ec2t attach-internet-gateway --internet-gateway-id $igw --vpc-id $vpc 
 
 #sg
 echo "creating security group"
 echo "======================="
-sg=`ec2 create-security-group --group-name nats_demo --description nats_demo --vpc-id $vpc |jq .GroupId -r`
+sg=`ec2 create-security-group --group-name nats_demo --description nats_demo --vpc-id $vpc |jq .GroupId `
 echo $sg
 
-retry ec2 authorize-security-group-ingress --group-id $sg --protocol -1 --from-port 0 --to-port 65535 --cidr 0.0.0.0/0
+ec2t authorize-security-group-ingress --group-id $sg --protocol -1 --from-port 0 --to-port 65535 --cidr 0.0.0.0/0
 
 #instances
 echo "launching instances"
 echo "======================="
 
-jbox=`ec2 run-instances --image-id    $nat_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_pub_a \
-    --private-ip-address 10.8.0.8 | jq .Instances[0].InstanceId -r`
-nat_a=`ec2 run-instances --image-id    $nat_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_pub_a \
-    --private-ip-address 10.8.0.5 | jq .Instances[0].InstanceId -r`
-nat_b=`ec2 run-instances --image-id    $nat_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_pub_a \
-    --private-ip-address 10.8.0.6 | jq .Instances[0].InstanceId -r`
+controller=`ec2 run-instances --image-id $nat_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_pub_a \
+    --private-ip-address 10.8.0.8 | jq .Instances[0].InstanceId `
+nat_a=`ec2 run-instances --image-id $nat_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_pub_a \
+    --private-ip-address 10.8.0.5 | jq .Instances[0].InstanceId `
+nat_b=`ec2 run-instances --image-id $nat_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_pub_a \
+    --private-ip-address 10.8.0.6 | jq .Instances[0].InstanceId `
 server_a=`ec2 run-instances --image-id $server_ami --count 1 --instance-type t1.micro --key-name bosh --security-group-ids $sg --subnet-id $subnet_prv_a \
-    --private-ip-address 10.8.2.9 | jq .Instances[0].InstanceId -r`
+    --private-ip-address 10.8.2.9 | jq .Instances[0].InstanceId `
 
-ec2 modify-instance-attribute --instance-id $nat_a --no-source-dest-check
-ec2 modify-instance-attribute --instance-id $nat_b --no-source-dest-check
+ec2t modify-instance-attribute --instance-id $nat_a --no-source-dest-check
+ec2t modify-instance-attribute --instance-id $nat_b --no-source-dest-check
 
-ec2 create-tags --resources $jbox          --tags Key=Name,Value=Nat_HA_Jumpbox
-ec2 create-tags --resources $nat_a         --tags Key=Name,Value=Nat_Box_A
-ec2 create-tags --resources $nat_b         --tags Key=Name,Value=Nat_Box_B
-ec2 create-tags --resources $server_a      --tags Key=Name,Value=Server_A
-
-eth_a=`ec2 describe-instances --instance-id $nat_a | jq .Reservations[].Instances[].NetworkInterfaces[].NetworkInterfaceId -r`
-eth_b=`ec2 describe-instances --instance-id $nat_b | jq .Reservations[].Instances[].NetworkInterfaces[].NetworkInterfaceId -r`
-
-ec2 assign-private-ip-addresses --network-interface-id $eth_a --private-ip-addresses 10.8.0.4 --allow-reassignment
+ec2t create-tags --resources $controller    --tags Key=Name,Value=Nat_HA_Control
+ec2t create-tags --resources $nat_a         --tags Key=Name,Value=Nat_Box_A
+ec2t create-tags --resources $nat_b         --tags Key=Name,Value=Nat_Box_B
+ec2t create-tags --resources $server_a      --tags Key=Name,Value=Server_A
 
 #elastic IP
 echo "allocating Elastic IPs"
 echo "======================="
 js=`ec2 allocate-address --domain vpc`
-eip_jb=`echo "$js" | jq .PublicIp -r`
-eipalloc_jb=`echo "$js" | jq .AllocationId -r`
-echo "Jumpbox IP: $eip_jb"
+eip_c=`echo "$js" | jq .PublicIp `
+eipalloc_c=`echo "$js" | jq .AllocationId`
 
 js=`ec2 allocate-address --domain vpc`
-eip_nat=`echo "$js" | jq .PublicIp -r`
-eipalloc_nat=`echo "$js" | jq .AllocationId -r`
+eip_nat=`echo "$js" | jq .PublicIp `
+eipalloc_nat=`echo "$js" | jq .AllocationId`
+
 echo "NAT egress IP: $eip_nat"
-
-js=`ec2 allocate-address --domain vpc`
-eip_a=`echo "$js" | jq .PublicIp -r`
-eipalloc_a=`echo "$js" | jq .AllocationId -r`
-echo "Nat_Box_A IP: $eip_a"
-
-js=`ec2 allocate-address --domain vpc`
-eip_b=`echo "$js" | jq .PublicIp -r`
-eipalloc_b=`echo "$js" | jq .AllocationId -r`
-echo "Nat_Box_B IP: $eip_b"
-
+echo "Controller IP: $eip_c"
 #attach EIPs
-ec2 associate-address --instance-id $jbox --allocation-id $eipalloc_jb
-ec2 associate-address --instance-id $nat_a --allocation-id $eipalloc_a
-ec2 associate-address --instance-id $nat_a --allocation-id $eipalloc_nat --private-ip-address 10.8.0.4
-ec2 associate-address --instance-id $nat_b --allocation-id $eipalloc_b
+ec2t associate-address --instance-id $controller --allocation-id $eipalloc_c
+ec2t associate-address --instance-id $nat_a --allocation-id $eipalloc_nat --allow-reassociation
 
 #route tables
-rt_main=`ec2 describe-route-tables --filter Name=vpc-id,Values=$vpc Name=association.main,Values=true | jq .RouteTables[].RouteTableId -r`
-rt_a=`ec2 create-route-table --vpc-id $vpc | jq .RouteTable.RouteTableId -r`
-rt_b=`ec2 create-route-table --vpc-id $vpc | jq .RouteTable.RouteTableId -r`
-ec2 create-tags --resources $rt_a          --tags Key=Name,Value=RT_A
-ec2 create-tags --resources $rt_b          --tags Key=Name,Value=RT_B
+rt_main=`ec2 describe-route-tables --filter Name=vpc-id,Values=$vpc Name=association.main,Values=true | jq .RouteTables[].RouteTableId `
+rt_a=`ec2 create-route-table --vpc-id $vpc | jq .RouteTable.RouteTableId `
+rt_b=`ec2 create-route-table --vpc-id $vpc | jq .RouteTable.RouteTableId `
+ec2t create-tags --resources $rt_a          --tags Key=Name,Value=RT_A
+ec2t create-tags --resources $rt_b          --tags Key=Name,Value=RT_B
 
-ec2 create-route --route-table-id $rt_main --destination-cidr-block 0.0.0.0/0 --gateway-id $igw
-ec2 create-route --route-table-id $rt_a --destination-cidr-block 0.0.0.0/0 --instance-id $nat_a
-ec2 create-route --route-table-id $rt_b --destination-cidr-block 0.0.0.0/0 --instance-id $nat_b
+ec2t create-route --route-table-id $rt_main --destination-cidr-block 0.0.0.0/0 --gateway-id $igw
+ec2t create-route --route-table-id $rt_a --destination-cidr-block 0.0.0.0/0 --instance-id $nat_a
+ec2t create-route --route-table-id $rt_b --destination-cidr-block 0.0.0.0/0 --instance-id $nat_b
 
-ec2 associate-route-table --subnet-id $subnet_prv_a --route-table-id $rt_a
+ec2t associate-route-table --subnet-id $subnet_prv_a --route-table-id $rt_a
 
 echo "Done"
-echo "run 'ssh -A ec2-user@$eip_jb' to play"
+echo "run 'ssh -A ec2-user@$eip_c' to play"
 echo "run 'nuke' to destroy the demo env"
 
 function nuke() {
 
-  ec2 terminate-instances --instance-ids $jbox $nat_a $nat_b $server_a
+  ec2t terminate-instances --instance-ids $controller $nat_a $nat_b $server_a
   sleep 60
-  ec2 release-address --allocation-id $eipalloc_jb
-  ec2 release-address --allocation-id $eipalloc_nat
+  ec2t release-address --allocation-id $eipalloc_c
+  ec2t release-address --allocation-id $eipalloc_nat
 
-  ec2 delete-security-group --group-id $sg
-  ec2 detach-internet-gateway --internet-gateway-id $igw --vpc-id $vpc
-  ec2 delete-internet-gateway --internet-gateway-id $igw
-  ec2 delete-route-table --route-table-id $rt_a
-  ec2 delete-route-table --route-table-id $rt_b
-  ec2 delete-subnet --subnet-id $subnet_pub_a
-  ec2 delete-subnet --subnet-id $subnet_prv_a
-  retry ec2 delete-vpc --vpc-id $vpc
-
-  unalias ec2
+  ec2t delete-security-group --group-id $sg
+  ec2t detach-internet-gateway --internet-gateway-id $igw --vpc-id $vpc
+  ec2t delete-internet-gateway --internet-gateway-id $igw
+  ec2t delete-subnet --subnet-id $subnet_pub_a
+  ec2t delete-subnet --subnet-id $subnet_prv_a
+  ec2t delete-route-table --route-table-id $rt_a
+  ec2t delete-route-table --route-table-id $rt_b
+  ec2t delete-vpc --vpc-id $vpc
 }
